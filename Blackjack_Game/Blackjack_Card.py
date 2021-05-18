d19d319caadc1feb180b6d773c6735c3b2bcaf3f
Rishi Rangarajan <61214686+rran9235@users.noreply.github.com>
2021-05-18 11:49:56 +0530

    Created classes for Card, Dealer and Player

---
+++ Blackjack_Game/Blackjack_Card.py
@@ -0,0 +1,21 @@
+"""
+MTRX5700 - Experimental Robotics
+Major Project - Blackjack Robot
+Year: 2021
+Group 5 - Curry Shop
+
+File:
+Info:
+"""
+
+# Imports
+
+
+# Class declaration
+class Blackjack_Card:
+    
+    def __init__(self, suit, number, value):
+
+        self.suit = suit
+        self.number = number
+        self.value = value
---
+++ Blackjack_Game/Blackjack_Dealer.py
@@ -0,0 +1,9 @@
+"""
+MTRX5700 - Experimental Robotics
+Major Project - Blackjack Robot
+Year: 2021
+Group 5 - Curry Shop
+
+File:
+Info:
+"""
